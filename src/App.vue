80159f16f06bab2a0b71e8a57c92988ba035844e
Edwin Rodriguez <edwin.rodz689@gmail.com>
2026-03-14 17:13:30 -0500

    Attribute bindings

--- src/App.vue
+++ src/App.vue
@@ -2,31 +2,16 @@
   <h1>{{ message }}</h1>
   <img v-bind:src="imageUrl" alt="" />
   <br />
-
-  <!-- shorthand for v-bind -->
   <img :src="imageUrl" alt="" />
-  <br />
 
   <button @click="changeImg">Change image</button>
-
-  <br />
-
   <hr />
-
   <input type="text" :value="defaultInputText" />
-
   <hr />
-
   <p :class="className">Harry Potter</p>
 
-  <!-- define a JS object in :class -->
-  <p :class="{ inactive: isInactive, center: isCenter }">
-    <!-- if you think embedding a JS object in HTML is verbose, you can choose to move the object to the script, 
-        give it a name, and only put the JS object name in :class -->
-    Harry Potter
-  </p>
+  <p :class="{ inactive: isInactive, center: isCenter }">Harry Potter</p>
 
-  <!-- define a JS array in :class -->
   <p :class="['active', 'center']">Harry Potter</p>
 </template>
 
